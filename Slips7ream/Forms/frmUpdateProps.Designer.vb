77e2c1ec4e2054ad28250ca7f68d1979d6e7859d
Andy <webmaster@RealityRipple.com>
2015-07-06 15:43:23 -0700

    Added Identity values to the Update Properties dialog.

--- Slips7ream/Forms/frmUpdateProps.Designer.vb
+++ Slips7ream/Forms/frmUpdateProps.Designer.vb
@@ -24,6 +24,8 @@ Partial Class frmUpdateProps
     Private Sub InitializeComponent()
     Me.components = New System.ComponentModel.Container()
     Me.pnlUpdateData = New System.Windows.Forms.TableLayoutPanel()
+    Me.lblIdentity = New System.Windows.Forms.Label()
+    Me.txtIdentity = New System.Windows.Forms.TextBox()
     Me.lblDisplayName = New System.Windows.Forms.Label()
     Me.txtDisplayName = New System.Windows.Forms.TextBox()
     Me.lblAppliesTo = New System.Windows.Forms.Label()
@@ -35,6 +37,10 @@ Partial Class frmUpdateProps
     Me.lblKBArticle = New System.Windows.Forms.Label()
     Me.lblKBLink = New Slips7ream.LinkLabel()
     Me.cmdClose = New System.Windows.Forms.Button()
+    Me.lblName = New System.Windows.Forms.Label()
+    Me.txtName = New System.Windows.Forms.TextBox()
+    Me.lblVersion = New System.Windows.Forms.Label()
+    Me.txtVersion = New System.Windows.Forms.TextBox()
     Me.ttLink = New Slips7ream.ToolTip(Me.components)
     Me.pnlUpdateData.SuspendLayout()
     Me.SuspendLayout()
@@ -44,114 +50,143 @@ Partial Class frmUpdateProps
     Me.pnlUpdateData.ColumnCount = 2
     Me.pnlUpdateData.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
     Me.pnlUpdateData.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-    Me.pnlUpdateData.Controls.Add(Me.lblDisplayName, 0, 0)
-    Me.pnlUpdateData.Controls.Add(Me.txtDisplayName, 1, 0)
-    Me.pnlUpdateData.Controls.Add(Me.lblAppliesTo, 0, 1)
-    Me.pnlUpdateData.Controls.Add(Me.txtAppliesTo, 1, 1)
-    Me.pnlUpdateData.Controls.Add(Me.lblArchitecture, 0, 2)
-    Me.pnlUpdateData.Controls.Add(Me.txtArchitecture, 1, 2)
-    Me.pnlUpdateData.Controls.Add(Me.lblBuildDate, 0, 3)
-    Me.pnlUpdateData.Controls.Add(Me.txtBuildDate, 1, 3)
-    Me.pnlUpdateData.Controls.Add(Me.lblKBArticle, 0, 4)
-    Me.pnlUpdateData.Controls.Add(Me.lblKBLink, 1, 4)
-    Me.pnlUpdateData.Controls.Add(Me.cmdClose, 1, 5)
+    Me.pnlUpdateData.Controls.Add(Me.lblIdentity, 0, 2)
+    Me.pnlUpdateData.Controls.Add(Me.txtIdentity, 1, 2)
+    Me.pnlUpdateData.Controls.Add(Me.lblDisplayName, 0, 1)
+    Me.pnlUpdateData.Controls.Add(Me.txtDisplayName, 1, 1)
+    Me.pnlUpdateData.Controls.Add(Me.lblAppliesTo, 0, 3)
+    Me.pnlUpdateData.Controls.Add(Me.txtAppliesTo, 1, 3)
+    Me.pnlUpdateData.Controls.Add(Me.lblArchitecture, 0, 4)
+    Me.pnlUpdateData.Controls.Add(Me.txtArchitecture, 1, 4)
+    Me.pnlUpdateData.Controls.Add(Me.lblBuildDate, 0, 5)
+    Me.pnlUpdateData.Controls.Add(Me.txtBuildDate, 1, 5)
+    Me.pnlUpdateData.Controls.Add(Me.lblKBArticle, 0, 7)
+    Me.pnlUpdateData.Controls.Add(Me.lblKBLink, 1, 7)
+    Me.pnlUpdateData.Controls.Add(Me.cmdClose, 1, 8)
+    Me.pnlUpdateData.Controls.Add(Me.lblName, 0, 0)
+    Me.pnlUpdateData.Controls.Add(Me.txtName, 1, 0)
+    Me.pnlUpdateData.Controls.Add(Me.lblVersion, 0, 6)
+    Me.pnlUpdateData.Controls.Add(Me.txtVersion, 1, 6)
     Me.pnlUpdateData.Dock = System.Windows.Forms.DockStyle.Fill
     Me.pnlUpdateData.Location = New System.Drawing.Point(0, 0)
     Me.pnlUpdateData.Name = "pnlUpdateData"
-    Me.pnlUpdateData.RowCount = 6
+    Me.pnlUpdateData.RowCount = 9
+    Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
+    Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
+    Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
+    Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
     Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
     Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
     Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
     Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
     Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-    Me.pnlUpdateData.RowStyles.Add(New System.Windows.Forms.RowStyle())
-    Me.pnlUpdateData.Size = New System.Drawing.Size(284, 156)
+    Me.pnlUpdateData.Size = New System.Drawing.Size(327, 236)
     Me.pnlUpdateData.TabIndex = 0
+    '
+    'lblIdentity
+    '
+    Me.lblIdentity.Anchor = System.Windows.Forms.AnchorStyles.Left
+    Me.lblIdentity.AutoSize = True
+    Me.lblIdentity.Location = New System.Drawing.Point(3, 58)
+    Me.lblIdentity.Name = "lblIdentity"
+    Me.lblIdentity.Size = New System.Drawing.Size(44, 13)
+    Me.lblIdentity.TabIndex = 4
+    Me.lblIdentity.Text = "Identity:"
+    '
+    'txtIdentity
+    '
+    Me.txtIdentity.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+    Me.txtIdentity.Location = New System.Drawing.Point(99, 55)
+    Me.txtIdentity.Name = "txtIdentity"
+    Me.txtIdentity.ReadOnly = True
+    Me.txtIdentity.Size = New System.Drawing.Size(225, 20)
+    Me.txtIdentity.TabIndex = 5
     '
     'lblDisplayName
     '
     Me.lblDisplayName.Anchor = System.Windows.Forms.AnchorStyles.Left
     Me.lblDisplayName.AutoSize = True
-    Me.lblDisplayName.Location = New System.Drawing.Point(3, 6)
+    Me.lblDisplayName.Location = New System.Drawing.Point(3, 32)
     Me.lblDisplayName.Name = "lblDisplayName"
     Me.lblDisplayName.Size = New System.Drawing.Size(75, 13)
-    Me.lblDisplayName.TabIndex = 0
+    Me.lblDisplayName.TabIndex = 2
     Me.lblDisplayName.Text = "Display Name:"
     '
     'txtDisplayName
     '
     Me.txtDisplayName.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.txtDisplayName.Location = New System.Drawing.Point(99, 3)
+    Me.txtDisplayName.Location = New System.Drawing.Point(99, 29)
     Me.txtDisplayName.Name = "txtDisplayName"
     Me.txtDisplayName.ReadOnly = True
-    Me.txtDisplayName.Size = New System.Drawing.Size(182, 20)
-    Me.txtDisplayName.TabIndex = 1
+    Me.txtDisplayName.Size = New System.Drawing.Size(225, 20)
+    Me.txtDisplayName.TabIndex = 3
     '
     'lblAppliesTo
     '
     Me.lblAppliesTo.Anchor = System.Windows.Forms.AnchorStyles.Left
     Me.lblAppliesTo.AutoSize = True
-    Me.lblAppliesTo.Location = New System.Drawing.Point(3, 32)
+    Me.lblAppliesTo.Location = New System.Drawing.Point(3, 84)
     Me.lblAppliesTo.Name = "lblAppliesTo"
     Me.lblAppliesTo.Size = New System.Drawing.Size(56, 13)
-    Me.lblAppliesTo.TabIndex = 2
+    Me.lblAppliesTo.TabIndex = 6
     Me.lblAppliesTo.Text = "Applies to:"
     '
     'txtAppliesTo
     '
     Me.txtAppliesTo.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.txtAppliesTo.Location = New System.Drawing.Point(99, 29)
+    Me.txtAppliesTo.Location = New System.Drawing.Point(99, 81)
     Me.txtAppliesTo.Name = "txtAppliesTo"
     Me.txtAppliesTo.ReadOnly = True
-    Me.txtAppliesTo.Size = New System.Drawing.Size(182, 20)
-    Me.txtAppliesTo.TabIndex = 3
+    Me.txtAppliesTo.Size = New System.Drawing.Size(225, 20)
+    Me.txtAppliesTo.TabIndex = 7
     '
     'lblArchitecture
     '
     Me.lblArchitecture.Anchor = System.Windows.Forms.AnchorStyles.Left
     Me.lblArchitecture.AutoSize = True
-    Me.lblArchitecture.Location = New System.Drawing.Point(3, 58)
+    Me.lblArchitecture.Location = New System.Drawing.Point(3, 110)
     Me.lblArchitecture.Name = "lblArchitecture"
     Me.lblArchitecture.Size = New System.Drawing.Size(67, 13)
-    Me.lblArchitecture.TabIndex = 4
+    Me.lblArchitecture.TabIndex = 8
     Me.lblArchitecture.Text = "Architecture:"
     '
     'txtArchitecture
     '
     Me.txtArchitecture.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.txtArchitecture.Location = New System.Drawing.Point(99, 55)
+    Me.txtArchitecture.Location = New System.Drawing.Point(99, 107)
     Me.txtArchitecture.Name = "txtArchitecture"
     Me.txtArchitecture.ReadOnly = True
-    Me.txtArchitecture.Size = New System.Drawing.Size(182, 20)
-    Me.txtArchitecture.TabIndex = 5
+    Me.txtArchitecture.Size = New System.Drawing.Size(225, 20)
+    Me.txtArchitecture.TabIndex = 9
     '
     'lblBuildDate
     '
     Me.lblBuildDate.Anchor = System.Windows.Forms.AnchorStyles.Left
     Me.lblBuildDate.AutoSize = True
-    Me.lblBuildDate.Location = New System.Drawing.Point(3, 84)
+    Me.lblBuildDate.Location = New System.Drawing.Point(3, 136)
     Me.lblBuildDate.Name = "lblBuildDate"
     Me.lblBuildDate.Size = New System.Drawing.Size(59, 13)
-    Me.lblBuildDate.TabIndex = 6
+    Me.lblBuildDate.TabIndex = 10
     Me.lblBuildDate.Text = "Build Date:"
     '
     'txtBuildDate
     '
     Me.txtBuildDate.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.txtBuildDate.Location = New System.Drawing.Point(99, 81)
+    Me.txtBuildDate.Location = New System.Drawing.Point(99, 133)
     Me.txtBuildDate.Name = "txtBuildDate"
     Me.txtBuildDate.ReadOnly = True
-    Me.txtBuildDate.Size = New System.Drawing.Size(182, 20)
-    Me.txtBuildDate.TabIndex = 7
+    Me.txtBuildDate.Size = New System.Drawing.Size(225, 20)
+    Me.txtBuildDate.TabIndex = 11
     '
     'lblKBArticle
     '
     Me.lblKBArticle.Anchor = System.Windows.Forms.AnchorStyles.Left
     Me.lblKBArticle.AutoSize = True
-    Me.lblKBArticle.Location = New System.Drawing.Point(3, 109)
+    Me.lblKBArticle.Location = New System.Drawing.Point(3, 188)
+    Me.lblKBArticle.Margin = New System.Windows.Forms.Padding(3, 6, 3, 7)
     Me.lblKBArticle.Name = "lblKBArticle"
     Me.lblKBArticle.Size = New System.Drawing.Size(90, 13)
-    Me.lblKBArticle.TabIndex = 8
+    Me.lblKBArticle.TabIndex = 14
     Me.lblKBArticle.Text = "Knowledge Base:"
     '
     'lblKBLink
@@ -161,25 +196,63 @@ Partial Class frmUpdateProps
     Me.lblKBLink.Cursor = System.Windows.Forms.Cursors.Hand
     Me.lblKBLink.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
     Me.lblKBLink.ForeColor = System.Drawing.Color.MediumBlue
-    Me.lblKBLink.Location = New System.Drawing.Point(99, 109)
+    Me.lblKBLink.Location = New System.Drawing.Point(99, 188)
     Me.lblKBLink.Name = "lblKBLink"
     Me.lblKBLink.Size = New System.Drawing.Size(45, 13)
-    Me.lblKBLink.TabIndex = 9
+    Me.lblKBLink.TabIndex = 15
     Me.lblKBLink.TabStop = True
     Me.lblKBLink.Text = "Article 0"
     Me.lblKBLink.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
     '
     'cmdClose
     '
-    Me.cmdClose.Anchor = System.Windows.Forms.AnchorStyles.Right
+    Me.cmdClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
     Me.cmdClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
     Me.cmdClose.FlatStyle = System.Windows.Forms.FlatStyle.System
-    Me.cmdClose.Location = New System.Drawing.Point(206, 130)
+    Me.cmdClose.Location = New System.Drawing.Point(249, 211)
     Me.cmdClose.Name = "cmdClose"
-    Me.cmdClose.Size = New System.Drawing.Size(75, 23)
-    Me.cmdClose.TabIndex = 10
+    Me.cmdClose.Size = New System.Drawing.Size(75, 22)
+    Me.cmdClose.TabIndex = 16
     Me.cmdClose.Text = "&Close"
     Me.cmdClose.UseVisualStyleBackColor = True
+    '
+    'lblName
+    '
+    Me.lblName.Anchor = System.Windows.Forms.AnchorStyles.Left
+    Me.lblName.AutoSize = True
+    Me.lblName.Location = New System.Drawing.Point(3, 6)
+    Me.lblName.Name = "lblName"
+    Me.lblName.Size = New System.Drawing.Size(38, 13)
+    Me.lblName.TabIndex = 0
+    Me.lblName.Text = "Name:"
+    '
+    'txtName
+    '
+    Me.txtName.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+    Me.txtName.Location = New System.Drawing.Point(99, 3)
+    Me.txtName.Name = "txtName"
+    Me.txtName.ReadOnly = True
+    Me.txtName.Size = New System.Drawing.Size(225, 20)
+    Me.txtName.TabIndex = 1
+    '
+    'lblVersion
+    '
+    Me.lblVersion.Anchor = System.Windows.Forms.AnchorStyles.Left
+    Me.lblVersion.AutoSize = True
+    Me.lblVersion.Location = New System.Drawing.Point(3, 162)
+    Me.lblVersion.Name = "lblVersion"
+    Me.lblVersion.Size = New System.Drawing.Size(45, 13)
+    Me.lblVersion.TabIndex = 12
+    Me.lblVersion.Text = "Version:"
+    '
+    'txtVersion
+    '
+    Me.txtVersion.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+    Me.txtVersion.Location = New System.Drawing.Point(99, 159)
+    Me.txtVersion.Name = "txtVersion"
+    Me.txtVersion.ReadOnly = True
+    Me.txtVersion.Size = New System.Drawing.Size(225, 20)
+    Me.txtVersion.TabIndex = 13
     '
     'frmUpdateProps
     '
@@ -187,12 +260,13 @@ Partial Class frmUpdateProps
     Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
     Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
     Me.CancelButton = Me.cmdClose
-    Me.ClientSize = New System.Drawing.Size(284, 156)
+    Me.ClientSize = New System.Drawing.Size(327, 236)
     Me.Controls.Add(Me.pnlUpdateData)
-    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
     Me.Icon = Global.Slips7ream.My.Resources.Resources.icon
     Me.MaximizeBox = False
     Me.MinimizeBox = False
+    Me.MinimumSize = New System.Drawing.Size(220, 270)
     Me.Name = "frmUpdateProps"
     Me.ShowIcon = False
     Me.ShowInTaskbar = False
@@ -216,4 +290,10 @@ Partial Class frmUpdateProps
   Friend WithEvents lblKBLink As LinkLabel
   Friend WithEvents cmdClose As System.Windows.Forms.Button
   Friend WithEvents ttLink As Slips7ream.ToolTip
+  Friend WithEvents lblIdentity As System.Windows.Forms.Label
+  Friend WithEvents txtIdentity As System.Windows.Forms.TextBox
+  Friend WithEvents lblName As System.Windows.Forms.Label
+  Friend WithEvents txtName As System.Windows.Forms.TextBox
+  Friend WithEvents lblVersion As System.Windows.Forms.Label
+  Friend WithEvents txtVersion As System.Windows.Forms.TextBox
 End Class
