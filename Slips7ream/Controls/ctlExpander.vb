2c4327e923fcc06c0f07a900b35e1cb2d3bddaa0
Andy <webmaster@RealityRipple.com>
2015-11-17 01:58:22 -0800

    Improved Expander. Improvements include mouse wheel support and optional text.

--- Slips7ream/Controls/ctlExpander.vb
+++ Slips7ream/Controls/ctlExpander.vb
@@ -14,7 +14,7 @@ Public Class Expander
   Public Event Opened(sender As Object, e As EventArgs)
   <Description("Event occurs when the expander has been closed.")>
   Public Event Closed(sender As Object, e As EventArgs)
-  <DefaultValue(True), Description("Indicates whether the component is in the open state.")>
+  <DefaultValue(False), Browsable(True), EditorBrowsable(True), Description("Indicates whether the component is in the open state.")>
   Public Property Open As Boolean
     Get
       Return c_Open
@@ -24,8 +24,7 @@ Public Class Expander
       DrawExpander(IIf(c_Open, EXP_OPEN, EXP_CLOSED))
     End Set
   End Property
-
-  <DefaultValue("Expander"), Browsable(True), EditorBrowsable(True), Description("The text associated with the control.")>
+  <DefaultValue("Expander"), Browsable(True), EditorBrowsable(True), Description("The text associated with the control."), DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)>
   Public Overrides Property Text As String
     Get
       Return c_Text
@@ -40,12 +39,13 @@ Public Class Expander
       End If
     End Set
   End Property
-
   Public Sub New()
-    ' This call is required by the designer.
     InitializeComponent()
-    ' Add any initialization after the InitializeComponent() call.
-    lblExpander.Text = c_Text
+    If Not String.IsNullOrEmpty(c_Text) Then
+      lblExpander.Text = c_Text
+    Else
+      lblExpander.Text = Nothing
+    End If
     If String.IsNullOrEmpty(lblExpander.Text) Then
       lblExpander.Padding = New Padding(0)
     Else
@@ -53,7 +53,6 @@ Public Class Expander
     End If
     DrawExpander(IIf(c_Open, EXP_OPEN, EXP_CLOSED) Or EXP_NORM)
   End Sub
-
   Public Sub PerformClick()
     c_Open = Not c_Open
     DrawExpander(IIf(c_Open, EXP_OPEN, EXP_CLOSED))
@@ -63,12 +62,12 @@ Public Class Expander
       RaiseEvent Closed(Me, New EventArgs)
     End If
   End Sub
-
   Private Sub pctExpander_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles pctExpander.MouseDown
+    If Not e.Button = Windows.Forms.MouseButtons.Left Then Return
     DrawExpander(EXP_DOWN)
   End Sub
-
   Private Sub pctExpander_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles pctExpander.MouseUp
+    If Not e.Button = Windows.Forms.MouseButtons.Left Then Return
     If c_Hover Or c_Focus Then
       DrawExpander(EXP_HOVER)
     Else
@@ -82,17 +81,14 @@ Public Class Expander
       RaiseEvent Closed(Me, New EventArgs)
     End If
   End Sub
-
   Private Sub pctExpander_MouseEnter(sender As Object, e As System.EventArgs) Handles pctExpander.MouseEnter
     c_Hover = True
     DrawExpander(EXP_HOVER)
   End Sub
-
   Private Sub pctExpander_MouseLeave(sender As Object, e As System.EventArgs) Handles pctExpander.MouseLeave
     c_Hover = False
     If Not c_Focus Then DrawExpander(EXP_NORM)
   End Sub
-
   Private Sub DrawExpander(iFlags As Integer)
     Static Last10 As Integer
     Static Last1 As Integer
@@ -116,7 +112,6 @@ Public Class Expander
       Last1 = (iFlags And &HF)
     End If
     If iItem = -1 Then iItem = 0
-
     Dim DrawOpenClosed As TriState = TriState.UseDefault
     If iFlags And EXP_OPEN Then
       DrawOpenClosed = TriState.True
@@ -132,16 +127,11 @@ Public Class Expander
     Else
       Last10 = (iFlags And &HF0)
     End If
-
     If DrawOpenClosed = TriState.True Then
-      'Open
       iItem += 3
     Else
-      'Closed
       iItem += 0
     End If
-
-
     Const Square As Integer = 19
     Using expanderChunk As New Bitmap(Square, Square)
       Using g As Graphics = Graphics.FromImage(expanderChunk)
@@ -151,22 +141,16 @@ Public Class Expander
       pctExpander.Image = expanderChunk.Clone
     End Using
     Application.DoEvents()
-
-
-
   End Sub
-
   Private Sub Expander_GotFocus(sender As Object, e As System.EventArgs) Handles Me.GotFocus
     c_Focus = True
     DrawExpander(EXP_HOVER)
   End Sub
-
   Private Sub Expander_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
     If e.KeyCode = Keys.Return Or e.KeyCode = Keys.Space Then
       DrawExpander(EXP_DOWN)
     End If
   End Sub
-
   Private Sub Expander_KeyUp(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
     If e.KeyCode = Keys.Return Or e.KeyCode = Keys.Space Then
       If c_Hover Or c_Focus Then
@@ -183,9 +167,19 @@ Public Class Expander
       End If
     End If
   End Sub
-
   Private Sub Expander_LostFocus(sender As Object, e As System.EventArgs) Handles Me.LostFocus
     c_Focus = False
     If Not c_Hover Then DrawExpander(EXP_NORM)
+  End Sub
+  Private Sub Expander_MouseWheel(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseWheel
+    If e.Delta < 0 And Not c_Open Then
+      c_Open = True
+      DrawExpander(EXP_OPEN)
+      RaiseEvent Opened(Me, New EventArgs)
+    ElseIf e.Delta > 0 And c_Open Then
+      c_Open = False
+      DrawExpander(EXP_CLOSED)
+      RaiseEvent Closed(Me, New EventArgs)
+    End If
   End Sub
 End Class
